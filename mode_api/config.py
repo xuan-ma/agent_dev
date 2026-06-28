23046a90c3d352b6d76e3c1052fd3ad20b2812c1
xuan-ma <maxuan0705@126.com>
2026-06-29 07:12:45 +0800

    Update model api

--- mode_api/config.py
+++ mode_api/config.py
@@ -7,13 +7,33 @@ models_mapping_info = {
             "OpenAI": "https://api.deepseek.com",
             "Anthropic": "https://api.deepseek.com/anthropic"
         },
+        "balance_url": "https://api.deepseek.com/user/balance",
         "model_name": "deepseek-v4-flash"  # deepseek-v4-pro
     },
     "OpenRouter": {
         "base_url": {
             "OpenAI": "https://openrouter.ai/api/v1",
         },
+        "balance_url": "https://openrouter.ai/api/v1/auth/key",
         "model_name": "openai/gpt-5"
+    },
+    "DashScope": {  # 阿里百炼
+        "base_url": {
+            "OpenAI": "https://dashscope.aliyuncs.com/compatible-mode/v1"
+        },
+        "model_name": "text-embedding-v3"
+    },
+    "ZhiPu": {
+        "base_url": {
+            "OpenAI": "https://open.bigmodel.cn/api/paas/v4/"
+        },
+        "model_name": "GLM-4.7"  # GLM-4.6V
+    },
+    "OpenAI": {
+        "base_url": {
+            "OpenAI": "https://api.openai.com/v1"
+        },
+        "model_name": "gpt-5"
     }
 }
 
--- mode_api/utils.py
+++ mode_api/utils.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 from config import model_type
 
-
+## 加载 apikey
 load_dotenv(override=True)
 api_key = os.getenv(f"{model_type.upper()}_API_KEY")
 print("api_key", api_key)
