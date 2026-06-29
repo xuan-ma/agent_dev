e482b017b2af817c368c68d376e5fe41c15b4354
xuan-ma <maxuan0705@126.com>
2026-06-30 07:24:57 +0800

    update mode api

--- mode_api/openai_method.py
+++ mode_api/openai_method.py
@@ -1,7 +1,8 @@
 from openai import OpenAI
 
-from config import model_info
 from utils import api_key
+from config import model_info
+
 from fee_info import token_consume
 
 client = OpenAI(
@@ -12,60 +13,21 @@ client = OpenAI(
 response = client.chat.completions.create(
     model=model_info["model_name"],  # "openai/gpt-5",  # deepseek-v4-flash
     messages=[
-        # user: 用户角色，代表人类的提问或输入
-        # assitent: 助手角色，代表 AI 的回复。在构造多轮对话时，需要手动添加历史回复
-        # system: 系统角色，用于设定 AI 的行为规范、角色定位、回复风格等。这是"幕后导演"，用户看不到，但会影响整个对话的基调。
+        # role:
+        #   - user: 用户角色，代表人类的提问或输入
+        #   - assitent: 助手角色，代表 AI 的回复。在构造多轮对话时，需要手动添加历史回复
+        #   - system: 系统角色，用于设定 AI 的行为规范、角色定位、回复风格等。这是"幕后导演"，用户看不到，但会影响整个对话的基调。
+        #   - tool: 调用工具, 需指定tool_call_id
+        #   - function: 调用函数, 需指定function name
         # {"role": "user", "content": "你好, 请用一句话介绍你自己"}
         {"role": "system", "content": "你是一位专业的 Python 编程导师，擅长用简洁明了的语言解释复杂概念。"},
         {"role": "user", "content": "什么是列表推导式？"}
-    ]
+    ],
+    temperature=0.7,
+    max_tokens=500,  # 控制模型输出长度
+    # max_completion_tokens=  这是控制什么输出
 )
 # print(type(response))
 token_consume(response)
 print("模型回复：", response.choices[0].message.content)
 print(type(response.model_dump_json()), response.model_dump_json())
-'''
-{
-    "id":"d96b8728-b7b5-4f42-925d-ab67ec5cdbed",
-    "choices":[
-        {
-            "finish_reason":"stop",
-            "index":0,
-            "logprobs":null,
-            "message":{
-                "content":"你好！我是DeepSeek，由深度求索公司创造的AI助手，免费提供高效、智能的对话、文件处理（支持图像、PDF、Word、Excel等）和联网搜索服务，拥有1M超长上下文，知识截止于2025年5月，致力于为你提供可靠、全面的帮助。",
-                "refusal":null,
-                "role":"assistant",
-                "annotations":null,
-                "audio":null,
-                "function_call":null,
-                "tool_calls":null,
-                "reasoning_content":"好的，用户让我用一句话介绍自己。这是一个非常简单的请求，需要简洁明了地概括我的核心身份和功能。我是DeepSeek，由深度求索公司创造，是一个AI助手。关键信息点包括：免费、多平台支持（网页和App）、文件处理能力、长上下文（1M）、知识截止日期和联网搜索选项。需要把这些整合成一句通顺自然的话，避免啰嗦。准备回复。"
-            }
-        }
-    ],
-    "created":1782399887,
-    "model":"deepseek-v4-flash",
-    "object":"chat.completion",
-    "moderation":null,
-    "service_tier":null,
-    "system_fingerprint":"fp_8b330d02d0_prod0820_fp8_kvcache_20260402",
-    "usage":{
-        "completion_tokens":158,
-        "prompt_tokens":12,
-        "total_tokens":170,
-        "completion_tokens_details":{
-            "accepted_prediction_tokens":null,
-            "audio_tokens":null,
-            "reasoning_tokens":91,
-            "rejected_prediction_tokens":null
-        },
-        "prompt_tokens_details":{
-            "audio_tokens":null,
-            "cached_tokens":0
-        },
-        "prompt_cache_hit_tokens":0,
-        "prompt_cache_miss_tokens":12
-    }
-}
-'''
--- mode_api/utils.py
+++ mode_api/utils.py
@@ -1,10 +1,13 @@
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 
 from config import model_type
 
 ## 加载 apikey
-load_dotenv(override=True)
+env_path = Path(__file__).parent / ".env"  # 当前文件同级目录
+load_dotenv(env_path, override=True)
 api_key = os.getenv(f"{model_type.upper()}_API_KEY")
+# api_key = os.getenv("DEEPSEEK_API_KEY")
 print("api_key", api_key)
